26d1c09eae968085e833c4cbd71e2b8d7a7edc53
Dmitry Bokhan <iteushm@gmail.com>
2021-05-16 23:00:50 +0300

    bug fix

--- Slider_infinity/js/script.js
+++ Slider_infinity/js/script.js
@@ -1,5 +1,6 @@
 let slides = document.querySelectorAll('.slide-single'); // получаем коллекцию документов изображений 
 let slider = []; //сюда будем сохранять src атрибуты изображений 
+let timeOffset = 2000; //время анимации в мс. 
 console.log(slides);
 
 for (let i = 0; i < slides.length; i++) {
@@ -11,17 +12,18 @@ console.log(slider);
 let step = 0; //шаг - определяет какую картинку показывать в текущий момент (по кругу) (контроль состояния слайдера)
 let offset = 0; //смещение изображения
 
-//фенкция отрисовки изображения (пока покажем только два)
+//фенкция отрисовки изображения (пока покажем только два слайда)
 function draw() {
-   let img = document.createElement('img'); // создаем элемент с классом img
+   console.log(step);
+
+   let img = document.createElement('img'); // создаем элемент img
    img.src = slider[step]; //присваиваем картинке src (это ссылка на картину которая в зависимости от step будет меняться)
    img.classList.add('slide-single'); //добавляем картинке класс 
 
    img.style.left = offset * 245 + 'px'; //зададим картинке смещение
-
+   img.style.transition = 'all ease ' + timeOffset / 1000 + 's';
    document.querySelector('#slider').appendChild(img);//получаем картинку и интегрируем ее внутрь блока
 
-   step++; //делаем шаг
 
    //проверяем можно ли наращивать шаг
    if (step + 1 == slider.length) {
@@ -32,20 +34,44 @@ function draw() {
    offset = 1; // делаем смещение
 }
 
-//
+//двигаем элементы
+let flag = false;
 function left() {
+   //пока функция не отработала до конца (пока не закончилась анимация и удаление крайней картинки)
+   //отключаем событие клика для документу
+   //document.onclick = null;
+   flag = false;
    let slideView = document.querySelectorAll('.slide-single'); //получим коллекцию видимых на данный момент картинок (в HTML)
    let offsetView = 0;
    for (let i = 0; i < slideView.length; i++) {
       slideView[i].style.left = offsetView * 245 - 245 + 'px';
+      offsetView++;
    }
+
+   //удаляем изображение после анимации 
+   setTimeout(function () {
+      slideView[0].remove(); //удаляем крайтнее левое изображение после смещения 
+      draw(); // добавляем новый элемент
+      //document.onclick = left;
+      flag = true;
+   }, timeOffset);
+
 }
 
 draw();
 draw();
+left(); //запускаем слайдер
+
+setInterval(function () {
+   if (flag) {
+      left();
+   }
+}, timeOffset);
 
 //для проверки вешаю событие клика по документу (потом убрать)
-document.onclick = left;
+//document.onclick = left;
+
+
 
 
 ;
